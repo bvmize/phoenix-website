e11be8f84658e29a25d9fdb7b7d9bb06308dbf55
Bridget Mizener <46944866+bvmize@users.noreply.github.com>
2022-12-08 21:16:11 -0500

    Update animated hero, add trophies

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # phoenix-website
-Phoenix Ultimate site with a long scrolling format. Track progress on <a href="https://trello.com/b/2ayyxlNK/phx-site">this</a> Trello board. Tweet
+Static Phoenix Ultimate site with a long scrolling format. Deployed on GitHub Pages. Track progress on <a href="https://trello.com/b/2ayyxlNK/phx-site">this</a> Trello board. Tweet
 us at @PHXUltimate with any updates / ideas on improving this site!
 
 
---
+++ script.js
@@ -0,0 +1,68 @@
+// Trophy Case Event Listeners
+function showTrophyDesc(e){
+    const display = document.querySelector("#trophy-display"); 
+    let id = e.target.getAttribute('id');
+    let year = `20${id[0]}${id[1]} `;
+    let trophyDesc = "";
+    let event = "";
+    switch (id[2]) {
+        case 'P':
+            event = "Pro Champs";
+            break;
+        case 'E':
+            event = "Elite Select";
+            break;
+        case 'N':
+            event = "USAU Nats";
+            break;
+        case "R":
+            event = "Regionals";
+            break;
+        case "W":
+            event = "WUCC";
+            break;
+        case "1":
+            event = "TCT Pro Flight";
+            break;
+        case "2":
+            event = "TCT Elite Flight";
+            break;
+        default:
+            event = "- no frisbee...";
+    }
+    if (id.length === 3) {
+        trophyDesc = year + event;
+    }
+    else {
+        let place = "";
+        switch (id[3]) {
+            case '1':
+                place = "1st @ ";
+                break;
+            case '2':
+                place = "2nd @ ";
+                break;
+            default:
+                place = "3rd @ ";
+                break;
+        }
+        trophyDesc = place + year + event;
+    }
+    display.textContent = trophyDesc;
+} 
+
+function hideTrophyDesc(){
+    const display = document.querySelector("#trophy-display");
+    display.textContent = "Take a look around!";   
+}
+
+function bindEventHandlers(){
+    const trophies = Array.from(document.querySelectorAll(".about-trophies span"));
+    for (let node of trophies) {
+        node.addEventListener('mouseover', showTrophyDesc);
+        node.addEventListener('mouseout', hideTrophyDesc);
+    }
+}
+
+//Bind Event Listeners
+addEventListener('load', bindEventHandlers);
